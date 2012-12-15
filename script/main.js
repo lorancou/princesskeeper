9fd221576f930a5adf17c12ce72a29ac65b0f2d3
Laurent Couvidou <lorancou@free.fr>
2012-12-15 17:14:36 +0100

    Code formatting

--- script/main.js
+++ script/main.js
@@ -2,6 +2,8 @@ var gamejs = require('gamejs');
 var box2d = require('Box2dWeb-2.1.a.3.min');
 var object = require('object');
 
+//------------------------------------------------------------------------------
+// preload everything, call main when done
 var data = [
     
     "../data/block00.png",
@@ -9,38 +11,47 @@ var data = [
     "../data/block02.png",
     "../data/block03.png",
 ];
-
 gamejs.preload(data);
 gamejs.ready(main);
 
+//------------------------------------------------------------------------------
 var NUM_BLOCK_KINDS = 4;
 var gBlockStore = new gamejs.sprite.Group();
-    
+
+//------------------------------------------------------------------------------
+// entry point
 function main() {
 
-    gamejs.display.setMode([1024, 768]);
+    init();
+    gamejs.time.fpsCallback(update, this, 60);
+}
 
+//------------------------------------------------------------------------------
+// create all gameplay elements
+function init() {
+
+    // set display size
+    gamejs.display.setMode([1024, 768]);
+    
+    // create block store
     gBlockStore = new gamejs.sprite.Group();
     for (var i=0; i<NUM_BLOCK_KINDS; i++) {
         gBlockStore.add(new object.block([32 + i*64, 32], i));
     }
-    
-    gamejs.time.fpsCallback(update, this, 60);
 }
-
+    
+//------------------------------------------------------------------------------
+// gather input then draw
 function update(msDuration) {
     
     input();
     draw();
-    
-    //console.log("update");
-    
-    return;
 }
 
+//------------------------------------------------------------------------------
+// handle key / mouse events
 function input() {
     
-    // handle key / mouse events
     gamejs.event.get().forEach(function(event) {
         if (event.type === gamejs.event.KEY_UP) {
             if (event.key === gamejs.event.K_UP) {
@@ -57,6 +68,8 @@ function input() {
     });
 }
 
+//------------------------------------------------------------------------------
+// blit
 function draw() {
     
     var mainSurface = gamejs.display.getSurface();
--- script/object.js
+++ script/object.js
@@ -1,20 +1,15 @@
 var gamejs = require('gamejs');
 
+//------------------------------------------------------------------------------
+// a tower block
 exports.block = function(position, index) {
     
     exports.block.superConstructor.apply(this, arguments);
 
-    /*this.originalImage = gamejs.transform.scale(
-                                this.originalImage,
-                                [dims[0] * (0.5 + Math.random()), dims[1] *  (0.5 + Math.random())]
-                        );*/
-
-                        // this.rotation = 50 + parseInt(120*Math.random());
     this.image = gamejs.image.load("../data/block0" + index + ".png");
     this.rect = new gamejs.Rect(position, this.image.getSize());
     this.index = index;
                         
     return this;
 };
-
 gamejs.utils.objects.extend(exports.block, gamejs.sprite.Sprite);
