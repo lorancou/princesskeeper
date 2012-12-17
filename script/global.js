1926e306380e5825bec5e5528f6403ead728f984
Laurent Couvidou <lorancou@free.fr>
2012-12-17 20:51:09 +0100

    Added loading feedback

--- script/global.js
+++ script/global.js
@@ -1,3 +1,4 @@
 //------------------------------------------------------------------------------
 exports.BOX2D_SCALE = 30.0;
-exports.DATA_PATH = "../data/"; // some hosts require "./data/"?!
+//exports.DATA_PATH = "../data/";
+exports.DATA_PATH = "./data/"; // some hosts require "./data/"?!
